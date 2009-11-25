3129c5210a41f49b007f9167b9d97d8ee71da9c7
Bevilacqua-Linn <mbevil0249k@DIVWL-9ACM642A.(none)>
2009-11-25 17:12:26 -0500

    Added ,become and ,who commands.

--- build/classes/com/randomfault/dopplecimmy.clj
+++ build/classes/com/randomfault/dopplecimmy.clj
@@ -2,7 +2,8 @@
  (:import
    [org.jibble.pircbot PircBot])
  (:require
-   [com.randomfault.markov :as brains])
+   [com.randomfault.markov :as brains]
+   [clojure.contrib.seq-utils :as seq])
  (:gen-class
    :extends org.jibble.pircbot.PircBot
    :init init
@@ -11,19 +12,43 @@
 (def nick "cimeleon")
 (def json "/Users/mbevil0249k/Downloads/milhouse.json")
 
+(def current (ref "mblinn"))
+
 (defn -init []
   (brains/parse-json json))
 
 (defn -post-init [this]
   (.setName this nick))
 
-(defn create-message [line nick]
-  (let [tokens (.split line " ")
-        first (list (nth tokens 1) (nth tokens 2))]
-    (do
-      (println (brains/create-sentence first "mblinn"))
-      (brains/create-sentence first "mblinn"))))
+(defn permutations [tokens]
+  (set (for [x tokens y tokens] (list x y))))
+
+(defmulti respond
+  (fn [tokens]
+    (cond
+      (.startsWith (first tokens) ",") :command
+      true :message)))
+
+(defmethod respond :message [tokens]
+  (let [possible-firsts (permutations tokens)]
+    (loop [possible-firsts possible-firsts]
+      (let [first (seq/rand-elt (seq possible-firsts))
+            sentence (brains/create-sentence first @current)]
+        (cond
+          (= 0 (count possible-firsts)) nil
+          (>= (count (.split sentence " ")) 5) sentence
+          true (recur (disj possible-firsts first)))))))
+
+(defmethod respond :command [tokens]
+  (cond
+    (.equalsIgnoreCase (first tokens) ",become")
+    (if (>= (count tokens) 2)
+      (dosync (ref-set current (nth tokens 1)))
+      "Who do you want me to be?")
+    (.equalsIgnoreCase (first tokens) ",who")
+    (format "I am %s" @current)))
 
 (defn -onMessage [this channel sender login hostname message]
   (if (.startsWith message nick)
-    (.sendMessage this "#milhouse" (create-message message "mblinn"))))
+    (let [response (respond (rest (.split message " ")))]
+      (.sendMessage this channel response))))
